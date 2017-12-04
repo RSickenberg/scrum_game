c9b51b078c03722ec73728dacc2ef3077bdf8fb8
Romain Sickenberg <romain.sickenberg@liip.ch>
2017-12-04 16:00:15 +0100

    Created the key push event on JS

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -5,3 +5,25 @@ function devShow() {
 function showExample() {
     window.open('example/index.html', '_blank');
 }
+
+function push(event) {
+    if (event.keyCode === 38 || event.keyCode === 87) {
+        // FOWARD
+    }
+
+    else if (event.keyCode === 40 || event.keyCode === 83) {
+        // DOWN
+    }
+
+    else if (event.keyCode === 39 || event.keyCode === 68) {
+        // RIGHT
+    }
+
+    else if (event.keyCode === 37 || event.keyCode === 65) {
+        // LEFT
+    }
+
+    else if (event.keyCode === 32) {
+        // UP
+    }
+}
