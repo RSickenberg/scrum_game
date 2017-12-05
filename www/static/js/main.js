8a233483f527b126ebc695c7b7771b507740f053
Romain Sickenberg <romain.sickenberg@liip.ch>
2017-12-05 20:52:08 +0100

    typo

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -1,4 +1,4 @@
-const DEPLACEMENT_VALUE = 7;
+const DISPLACEMENT_VALUE = 7;
 
 document.onkeydown = pushKey;
 
@@ -13,25 +13,25 @@ function pushKey(event) {
     if (event.keyCode === 38 || event.keyCode === 87) {
         // FORWARD
         var top = parseInt(element.style.top);
-        element.style.top = top - DEPLACEMENT_VALUE + 'px';
+        element.style.top = top - DISPLACEMENT_VALUE + 'px';
     }
 
     if (event.keyCode === 40 || event.keyCode === 83) {
         // DOWN
         var down = parseInt(element.style.top);
-        element.style.top = down + DEPLACEMENT_VALUE + 'px';
+        element.style.top = down + DISPLACEMENT_VALUE + 'px';
     }
 
     if (event.keyCode === 39 || event.keyCode === 68) {
         // RIGHT
         var right = parseInt(element.style.left);
-        element.style.left = right + DEPLACEMENT_VALUE + 'px';
+        element.style.left = right + DISPLACEMENT_VALUE + 'px';
     }
 
     if (event.keyCode === 37 || event.keyCode === 65) {
         // LEFT
         var left = parseInt(element.style.left);
-        element.style.left = left - DEPLACEMENT_VALUE + 'px';
+        element.style.left = left - DISPLACEMENT_VALUE + 'px';
         console.log(event);
     }
 
