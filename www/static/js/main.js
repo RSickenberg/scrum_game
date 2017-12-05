fec2e055542511169e280d5bf0902ace5bcd644b
Adry Valverde <adrian.valverde.torres.97@gmail.com>
2017-12-05 23:23:17 +0100

    Ajouts de fleches

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -1,7 +1,36 @@
+const DEPLACEMENT_VALUE = "10px";
+
 function devShow() {
     window.open('pages/components.html', '_blank');
 }
 
 function showExample() {
     window.open('example/index.html', '_blank');
+}
+
+function appuye(event) {
+    if (event.keyCode == 38 || event.keyCode == 87) {
+        // FORWARD
+        console.log(event);
+    }
+
+    if (event.keyCode == 40 || event.keyCode == 83) {
+        // DOWN
+        console.log(event);
+    }
+
+    if (event.keyCode == 39 || event.keyCode == 68) {
+        // RIGHT
+        console.log(event);
+    }
+
+    if (event.keyCode == 37 || event.keyCode == 65) {
+        // LEFT
+        console.log(event);
+    }
+
+    if (event.keyCode == 32) {
+        // UP
+        console.log(event);
+    }
 }
