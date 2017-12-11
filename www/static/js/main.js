40800477e1067a038e773c30e13ad96fe7514f78
Adry Valverde <adrian.valverde.torres.97@gmail.com>
2017-12-11 12:10:22 +0100

    Clean wrong merge

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -1,47 +1,15 @@
-<<<<<<< HEAD
-const DEPLACEMENT_VALUE = "10px";
-=======
 const DISPLACEMENT_VALUE = 7;
 
 document.onkeydown = pushKey;
->>>>>>> f41e474792b04243cd462b9d9b849c55609bbe34
 
 function devShow() {
     window.open('pages/components.html', '_blank');
 }
 
-<<<<<<< HEAD
 function showExample() {
     window.open('example/index.html', '_blank');
 }
 
-function appuye(event) {
-    if (event.keyCode == 38 || event.keyCode == 87) {
-        // FORWARD
-        console.log(event);
-    }
-
-    if (event.keyCode == 40 || event.keyCode == 83) {
-        // DOWN
-        console.log(event);
-    }
-
-    if (event.keyCode == 39 || event.keyCode == 68) {
-        // RIGHT
-        console.log(event);
-    }
-
-    if (event.keyCode == 37 || event.keyCode == 65) {
-        // LEFT
-        console.log(event);
-    }
-
-    if (event.keyCode == 32) {
-        // UP
-        console.log(event);
-    }
-}
-=======
 function pushKey(event) {
 
     var element = document.getElementById('exampleGuy');
@@ -84,9 +52,4 @@ function pushKey(event) {
     }
 }
 
-
->>>>>>> f41e474792b04243cd462b9d9b849c55609bbe34
-
 ////////////// PAGE GAME /////////////
-
-
